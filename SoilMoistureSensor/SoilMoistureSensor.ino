180bb0eef4901b40dcdd000ab98bfd7f27ff4fa9
Kevin Covey <entropist65@gmail.com>
2014-11-19 20:50:38 -0600

    Sense resistance and display

--- SoilMoistureSensor/SoilMoistureSensor.ino
+++ SoilMoistureSensor/SoilMoistureSensor.ino
@@ -1,14 +1,19 @@
 #include <MicroView.h>
 
+int moistureSensor = 0;
+int moistureValue;
+
 void setup() {
   uView.begin();
   uView.clear(PAGE);
-  uView.println();
-  uView.println();
-  uView.println("Hello");
-  uView.println("World!");
-  uView.display();
+  uView.setFontType(3);
 }
 
 void loop() {
+  moistureValue = analogRead(moistureSensor);
+  uView.clear();
+  uView.setCursor(0, 0);
+  uView.print(moistureValue);
+  uView.display();
+  delay(500);
 }
